0ffb2bc91e7ee9aa4398f3e5c19abd8f5692ab93
meeej <jimchiang001@gmail.com>
2017-09-08 16:22:06 -0700

    add butterknife and test button

--- app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
+++ app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.jimchiang.shopify_challenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_submit) Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "shopify!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
