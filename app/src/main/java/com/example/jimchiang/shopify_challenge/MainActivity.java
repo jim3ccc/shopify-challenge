f5e2b1f3d149fbb74ce70eb47347f6da5b6ec910
meeej <jimchiang001@gmail.com>
2017-09-08 16:39:31 -0700

    get value from editText

--- app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
+++ app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.jimchiang.shopify_challenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.btn_submit) Button mSubmitButton;
+    @BindView(R.id.editText) EditText mEditTextString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "shopify!", Toast.LENGTH_SHORT).show();
+                //When we click submit, we get value from editText and spinner
+                String editText = mEditTextString.getText().toString();
+                Log.d(TAG, "editText value: " + editText);
+
+
             }
         });
     }
