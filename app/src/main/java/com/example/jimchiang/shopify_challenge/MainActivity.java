9f95d0c62bdc6000fdb13b4ca4045292435ebccf
meeej <jimchiang001@gmail.com>
2017-09-09 08:54:35 -0700

    create and set adapter for spinner

--- app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
+++ app/src/main/java/com/example/jimchiang/shopify_challenge/MainActivity.java
@@ -4,18 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.btn_submit) Button mSubmitButton;
     @BindView(R.id.editText) EditText mEditTextString;
+    @BindView(R.id.spinner_user_choices) Spinner mSpinnerUserChoices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        //set adapter for spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.user_choices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerUserChoices.setAdapter(adapter);
+
+        //set listener for spinner
+        mSpinnerUserChoices.setOnItemSelectedListener(this);
+
+        //Button onClick
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +47,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
